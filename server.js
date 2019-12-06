622fe593dadd91093b5c30e2e1b904c995437d6f
Clarence Ondieki <condieki@Clarences-MacBook-Pro.local>
2019-12-06 18:07:50 -0500

    oac embedding example

---
+++ server.js
@@ -0,0 +1,31 @@
+//opioid workers
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  //res.send('Hello World!')
+  res.render('home');
+
+})
+
+
+
+
+app.get('/home', function (req, res) {
+	//res.send('Hello World!')
+	res.render('index');
+  })
+
+app.post('/', function (req, res) {
+	res.render('index');
+  console.log(req.body.city);
+  })
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
